091c75e75ebf547e81301f4b1c1e1b48ce3dc01b
Xiaoqiu HE <hexiaoqiu@gmail.com>
2017-09-18 16:11:02 +0800

    correct some errors

--- test2.m
+++ test2.m
@@ -41,10 +41,10 @@ pstnTable = zeros(119,4);
 idxG = 1;
 for idxY = 1:1:16
         for idxZ = 1:1:6
-                pstnTable(idxG,1) = Y_undim(1,idxY);
-                pstnTable(idxG,2) = Z_undim(1,idxZ);
-                pstnTable(idxG,3) = (Y_undim(1,idxY) * H/2 + H);
-                pstnTable(idxG,4) = (Z_undim(1,idxZ) * H/2 + H/2);
+                pstnTable(idxG,1) = Y_undim(idxY,1);
+                pstnTable(idxG,2) = Z_undim(idxZ,1);
+                pstnTable(idxG,3) = (Y_undim(idxY,1) * H/2 + H);
+                pstnTable(idxG,4) = (Z_undim(idxZ,1) * H/2 + H/2);
                 idxG = idxG + 1;
         end
 end
@@ -74,12 +74,12 @@ initCd(5,1) = deltaT_0;
 %                       Step 3     Running  iterations                                    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for idxG = 1:1:16
-        Yp = testResult(idxG,3);        
-        Zp = testResult(idxG,4);
-        testResult(idxG,5:21) = Get1PointFi(Yp, Zp, initCd, flowModel);
-        LiuChao_FMshGStr0 = testResult;
-        save('LiuChao_FMshGStr0','LiuChao_FMshGStr0');  
-        quiver(LiuChao_FMshGStr0(:,1), LiuChao_FMshGStr0(:,2), LiuChao_FMshGStr0(:,11), LiuChao_FMshGStr0(:,12));     
+                Yp              = testResult(idxG,3);        
+                Zp              = testResult(idxG,4);
+        testResult(idxG,5: 21)  = Get1PointFi(Yp, Zp, initCd, flowModel);
+        LiuChao_NMshGStr0       = testResult;
+        save('LiuChao_NMeshGStr0','LiuChao_NMeshGStr0');  
+        quiver(LiuChao_NMeshGStr0(:,1), LiuChao_NMeshGStr0(:,2), LiuChao_NMeshGStr0(:,11), LiuChao_NMeshGStr0(:,12));     
 end
 
 
